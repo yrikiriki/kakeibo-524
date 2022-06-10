0477ff19b0ae39441a1ec499c0641c937a8add6e
yrikiriki <toyota.30524@gmail.com>
2022-06-10 11:35:08 +0900

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
   private
-
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'riki' && password == '0524'
+    end
+  end
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
